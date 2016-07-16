74c34a87a5f21a2833d3b6064a85a2b666b2ac5b
JulienBe <bertozzijulien@gmail.com>
2016-07-16 22:34:48 +0200

    Utils method

--- core/src/main/scala/brols/Creator.scala
+++ core/src/main/scala/brols/Creator.scala
@@ -7,13 +7,21 @@ import scala.util.Random
   */
 object Creator {
 
-  def norm() = Random.nextFloat()
-
+  def float = Random.nextFloat
+  def boolean = Random.nextBoolean
+  def int(max: Int) = Random.nextInt(max)
   def valueInBounds(min: Float, max: Float) = min + (Random.nextFloat() * (max - min))
 
   /**
     * >= min && < max
     */
   def testBound(min: Float, max: Float, value: Float) = value >= min && value < max
+
+  def randomIntNot(ceiling: Int, not: Int) = {
+    var rep = Random.nextInt(ceiling)
+    if (rep == not)
+      rep = (rep + 1) % ceiling
+    rep
+  }
 
 }
